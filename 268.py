5ed3318f82e9724ab16f69dd565a3080a6479fc2
sophiez <sophiez.89@outlook.com>
2023-04-24 01:27:21 -0700

    missing number solution

---
+++ 268.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        total = sum(nums)
+        result = 0
+        for i in range(len(nums) + 1):
+            result += i
+        return result - total
