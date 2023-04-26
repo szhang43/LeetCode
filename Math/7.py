0a7d5513b37f5743414e115f28730a3fff6a1022
sophiez <sophiez.89@outlook.com>
2023-04-26 00:40:24 -0700

    reverse number

--- Math/7.py
+++ Math/7.py
@@ -1,15 +1,14 @@
 class Solution:
     def reverse(self, x: int) -> int:
         num = 0
-        n = x
         negative = 0
-        if n < 0: 
+        if x < 0: # checking for negative
             negative = 1
-            n *= -1
-        while(n != 0):
-            num *= 10 
-            num += n % 10
-            n = n // 10 # Get the number before the decimal and round it up
+            x *= -1
+        while(x != 0):
+            num *= 10 # add a place holder to add the next value
+            num += x % 10 # gets the last digit
+            x = x // 10 # removes the last digit
         if negative:
             num *= -1
         if num < -2**31 or num > 2**31 - 1:
