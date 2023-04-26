eec1d883180126e4a02362b70bb8334aca3d43a1
sophiez <sophiez.89@outlook.com>
2023-04-26 00:42:24 -0700

    updated files

--- Math/268.py
+++ Math/268.py
@@ -1,7 +1,10 @@
 class Solution:
+    #First Attempt
     def missingNumber(self, nums: List[int]) -> int:
         total = sum(nums)
         result = 0
         for i in range(len(nums) + 1):
             result += i
         return result - total
+
+    
--- Math/342.py
+++ Math/342.py
@@ -7,3 +7,4 @@ class Solution:
             else: 
                 i += 1
         return False
+
