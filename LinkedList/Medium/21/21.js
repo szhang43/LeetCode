feccd018f0ecffa1f6847be41f25486505456fab
Sophie <sophiez.89@outlook.com>
2024-01-11 21:38:24 -0800

    Merge 2 sorted linked list

---
+++ LinkedList/Medium/21/21.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+
+
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode(); 
+    let curr = dummy; 
+  
+    console.log(curr)
+    while(list1 && list2){
+        if(list1.val < list2.val){
+            curr.next = list1;
+            list1 = list1.next;
+        } else {
+            curr.next = list2
+            list2 = list2.next;
+        }
+        curr = curr.next
+    }
+    
+    if(list1 !== null){
+        curr.next = list1
+    }
+    if(list2 !== null){
+        curr.next = list2;
+    }
+    return dummy.next;
+};
